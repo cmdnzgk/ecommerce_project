9350f50ed938baefb5fed2fc8bc2c5a52d6cbb56
cmdnzgk <cemdenizgok@hotmail.com>
2026-05-04 04:11:55 +0300

    feat: completed ecommerce core with cart, quantity management and product detail

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("", views.product_list, name="product_list"),
     path("product/<int:id>/", views.product_detail, name="product_detail"),
     path("add_to_cart/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("cart/", views.cart_view, name="cart"),
+    path("increase/<int:item_id>/", views.increase_quantity, name="increase_quantity"),
+    path("decrease/<int:item_id>/", views.decrease_quantity, name="decrease_quantity"),
+    path("remove/<int:item_id>/", views.remove_from_cart, name="remove_from_cart"),
 ]
--- store/views.py
+++ store/views.py
@@ -27,13 +27,49 @@ def add_to_cart(request, product_id):
 
     cart, created = Cart.objects.get_or_create(user = request.user)
 
-    cart_item, created = CartItem.objects.get_or_create(
-        cart = cart,
-        product = product
-    )
+    cart_item, created = CartItem.objects.get_or_create(cart = cart, product = product)
 
     if not created:
         cart_item.quantity += 1
         cart_item.save()
 
     return redirect("product_list")
+
+login_required
+def cart_view(request):
+    cart, created = Cart.objects.get_or_create(user = request.user)
+    cart_items = CartItem.objects.filter(cart = cart)
+
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+
+    return render(request, "cart.html", {
+        "cart_items": cart_items,
+        "total_price": total_price
+    })
+
+login_required
+def increase_quantity(request, item_id):
+    item = CartItem.objects.get(id = item_id)
+    item.quantity += 1
+    item.save()
+
+    return redirect("cart")
+
+login_required
+def decrease_quantity(request, item_id):
+    item = CartItem.objects.get(id = item_id)
+
+    if item.quantity > 1:
+        item.quantity -= 1
+        item.save()
+    else:
+        item.delete()
+    
+    return redirect("cart")
+
+login_required
+def remove_from_cart(request, item_id):
+    item = CartItem.objects.get(id = item_id)
+    item.delete()
+
+    return redirect("cart")
